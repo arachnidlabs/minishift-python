84b4fc8fdc3808340293c076a1628bf0decd2d2c
Nick Johnson <arachnid@notdot.net>
2014-04-17 12:53:21 +0100

    Add python-daemon as a dep

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name="minishift-python",
-      version="0.1.2",
+      version="0.1.3",
       description="Python interface for the minishift",
       author="Nick Johnson",
       author_email="nick@arachnidlabs.com",
       url="https://github.com/arachnidlabs/minishift-python/",
       packages=["minishift"],
-      requires=["mcp2210"])
+      install_requires=["mcp2210", "python-daemon"])
